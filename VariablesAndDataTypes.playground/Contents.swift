220f3bbe24c024f0ac5c8177e532a1bbfce0d3b7
Marcelo Amador <marcelojaf@gmail.com>
2022-10-04 15:18:24 -0300

    Pushing iOS 11 course content

---
+++ VariablesAndDataTypes.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var myAge : Int = 32
+
+myAge = 38
+
+let myName : String = "Marcelo"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) Amador"
+
+let myDetails = "\(myName), \(myAge)"
